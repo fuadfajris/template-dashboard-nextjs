e087056eb0deb1e220c01e13a7f0d4e289008c72
fuad.fajri <fuad.fajri@bni.co.id>
2025-08-27 10:39:25 +0700

    update icon and image merchant

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -4,6 +4,8 @@ import GroupIcon from "./group.svg";
 import MoreDotIcon from "./more-dot.svg";
 import GridIcon from "./grid.svg";
 import CalendarIcon from "./calendar.svg";
+import BoxIconLine from "./box-line.svg";
+import TicketIcon from "./ticket.svg";
 
 export {
   ChevronDownIcon,
@@ -12,4 +14,6 @@ export {
   MoreDotIcon,
   GridIcon,
   CalendarIcon,
+  BoxIconLine,
+  TicketIcon,
 };
